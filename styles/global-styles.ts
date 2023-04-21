cf87db713bfce1adeb64a37e14fc006d81a65e25
jong-k <kjhan710@naver.com>
2023-04-21 15:30:04 +0900

    chore: styled-normalize 추가

--- styles/global-styles.ts
+++ styles/global-styles.ts
@@ -1,6 +1,8 @@
 import { createGlobalStyle } from "styled-components";
+import { normalize } from "styled-normalize";
 
 const globalStyles = createGlobalStyle`
+  ${normalize}
   @font-face {
     font-family: "Pretendard-Regular";
     src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
