200061e952952490ba2142645e8b11aa0d88e9c7
taejinii <xowls177@gmail.com>
2023-04-25 15:38:37 +0900

    design: Theme.ts 수정

--- styles/Theme.ts
+++ styles/Theme.ts
@@ -1,28 +1,11 @@
 export const theme = {
   colors: {
-    primary: "#19A7CE",
-    secondary: "#146C94",
-    tertiary: "#303030",
-    background: "#ffffff",
-    text: "#000000",
-    primaryTextColor: "#594F43",
-    secondaryTextColor: "#777777",
-    inputPlaceholder: "#C7C7C7",
-    darkGrayText: "#303030",
-    darkText: "#1A1A1A",
-    black: "#000000",
-    white: "#ffffff",
-    dark: "",
-    medium: "",
-    light: "",
-    danger: "",
-    success: "#66A15A",
+    primary: "#155EEF",
+    gray: "#98A2B3",
+    danger: "#F04438",
+    success: "#16B364",
   },
   fonts: {
     pretendard: "Pretendard-Regular",
-  },
-  viewport: {
-    medium: "768px",
-    large: "1360px",
   },
 };
