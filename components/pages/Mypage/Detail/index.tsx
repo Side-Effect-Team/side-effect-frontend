f6071e6be77e7892dcabf12e0fd159455bac7a3d
한주연 <hjy2859@naver.com>
2023-04-29 02:39:26 +0900

    refactor: 코드 리팩토링 및 예시 데이터 생성

---
+++ components/pages/Mypage/Detail/index.tsx
@@ -0,0 +1,88 @@
+import Button from "../../../Button";
+import {
+  Border,
+  ContentLink,
+  ContentNum,
+  ContentTitle,
+  ContentsList,
+  InfoContent,
+  InfoTitle,
+  InfoWrapper,
+  NickName,
+  ProfileImage,
+  ProfileWrapper,
+  SectionHeaderWrapper,
+  SectionTitle,
+  SectionWrapper,
+  Tag,
+  TagWrapper,
+  Text,
+  Wrapper,
+} from "./styled";
+
+interface DataProps {
+  avatarSrc?: string;
+  nickname: string;
+  introduction?: string;
+  followInfo: {
+    title: string;
+    num: number;
+  }[];
+  skill?: string[];
+  info?: {
+    title: string;
+    content: string;
+  }[];
+}
+interface MyPageDetailProps {
+  data?: DataProps;
+}
+export default function MyPageDetail(p: MyPageDetailProps) {
+  return (
+    <Wrapper>
+      <ProfileWrapper>
+        <ProfileImage
+          src={p.data?.avatarSrc || "/img/BoardDefaultBackground.png"}
+        />
+        <NickName>{p.data?.nickname || ""}</NickName>
+        <Text>{p.data?.introduction || "아직 소개가 없습니다."}</Text>
+        <ContentsList>
+          {p.data?.followInfo.map((el, index) => (
+            <ContentLink key={index}>
+              <ContentNum>{el.num}</ContentNum>
+              <ContentTitle>{el.title}</ContentTitle>
+            </ContentLink>
+          ))}
+        </ContentsList>
+        <Button>프로필 수정하기</Button>
+      </ProfileWrapper>
+      <SectionWrapper>
+        <SectionHeaderWrapper>
+          <SectionTitle>Skill</SectionTitle>
+          <Border></Border>
+        </SectionHeaderWrapper>
+        <TagWrapper>
+          {p.data?.skill?.map((el, index) => <Tag key={index}>{el}</Tag>) || (
+            <Text>아직 작성된 정보가 없습니다.</Text>
+          )}
+        </TagWrapper>
+      </SectionWrapper>
+      <SectionWrapper>
+        <SectionHeaderWrapper>
+          <SectionTitle>Info</SectionTitle>
+          <Border></Border>
+        </SectionHeaderWrapper>
+        {p.data?.info ? (
+          p.data.info.map((el, index) => (
+            <InfoWrapper key={index}>
+              <InfoTitle>{el.title}</InfoTitle>
+              <InfoContent>{el.content}</InfoContent>
+            </InfoWrapper>
+          ))
+        ) : (
+          <Text>아직 작성된 정보가 없습니다.</Text>
+        )}
+      </SectionWrapper>
+    </Wrapper>
+  );
+}
--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -1,5 +1,24 @@
-import MyPageDetail from "../../components/Mypage/Detail";
+import MyPageDetail from "../../components/pages/Mypage/Detail";
 
 export default function MyPage() {
-  return <MyPageDetail />;
+  const data = {
+    avatarSrc: "/img/ProjectDefaultBackground.png",
+    nickname: "자라는개발자",
+    introduction:
+      "프론트엔드 개발자를 꿈꾸는 취준생입니다. 프로젝트 경험하고 싶어요",
+    followInfo: [
+      { title: "게시물", num: 1 },
+      { title: "팔로워", num: 20 },
+      { title: "팔로잉", num: 30 },
+    ],
+    skill: ["typescript", "react", "HTML", "Next.js", "React.native"],
+    info: [
+      { title: "포지션", content: "프론트엔드" },
+      { title: "경력", content: "취업준비생" },
+      { title: "깃허브", content: "www.abc.com" },
+      { title: "블로그", content: "www.abc.com" },
+      { title: "포트폴리오", content: "www.abc.com" },
+    ],
+  };
+  return <MyPageDetail data={data} />;
 }
