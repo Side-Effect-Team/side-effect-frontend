f3420dcb74a989aac112c8827c63132ece370aec
한주연 <hjy2859@naver.com>
2023-05-09 15:19:20 +0900

    update: 검색 Api를 받아오는것에서 keyword만 전송해주는 것으로 변경(디바운싱 적용완료) #42

--- components/Search/index.tsx
+++ components/Search/index.tsx
@@ -7,55 +7,26 @@ import {
   useState,
 } from "react";
 import { SearchDiv, StyledInput } from "./styled";
-import axios from "axios";
 
 type InputProps = InputHTMLAttributes<HTMLInputElement>;
 
 interface SearchProps extends InputProps {
-  setData: Dispatch<SetStateAction<any>>;
-  category: string;
+  setKeyword: Dispatch<SetStateAction<string>>;
 }
 
-export default function Search({ setData, category, ...rest }: SearchProps) {
-  const [keyword, setKeyword] = useState("");
+export default function Search({ setKeyword }: SearchProps) {
+  const [searchKeyword, setSearchKeyword] = useState("");
 
   const onChangeKeyword = (e: ChangeEvent<HTMLInputElement>) => {
-    setKeyword(e.currentTarget.value);
-  };
-
-  const fetchRecruits = async () => {
-    try {
-      const result = await axios.get(
-        `http://54.64.103.42:8080/api/recruit-board/scroll?&size=10&keyword=${keyword}`,
-      );
-      setData(result.data);
-    } catch (error) {
-      setData(null);
-      console.log(error);
-    }
-  };
-  const fetchProjects = async () => {
-    try {
-      const result = await axios.get(
-        `http://54.64.103.42:8080/api/free-boards/search?&size=10&keyWord=${keyword}`,
-      );
-      setData(result.data);
-    } catch (error) {
-      setData(null);
-      console.log(error);
-    }
+    setSearchKeyword(e.currentTarget.value);
   };
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      if (category === "recruits") {
-        fetchRecruits();
-      } else {
-        fetchProjects();
-      }
+      setKeyword(searchKeyword);
     }, 300);
     return () => clearTimeout(timer);
-  }, [keyword]);
+  }, [searchKeyword]);
 
   return (
     <SearchDiv>
@@ -63,12 +34,11 @@ export default function Search({ setData, category, ...rest }: SearchProps) {
         type="text"
         placeholder="검색어를 입력하세요."
         onChange={onChangeKeyword}
-        {...rest}
       />
     </SearchDiv>
   );
 }
 
 // 쓰는 방법
-// 데이터 받아주시고 아래의 양식처럼 props 보내주시면 됩니다.
-// <Search category="recruits" setData={setData} /> or <Search category="projects" setData={setData} />
+// const [keyword, setKeyword] = useState(""); 페이지에서 선언 후 아래 양식 처럼 props 보내주시면 됩니다.
+// <Search setKeyword={setKeyword} />
