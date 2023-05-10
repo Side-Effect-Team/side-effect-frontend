f3c0b5ceadca1d6bace8c254221c54e752757c61
taejinii <xowls177@gmail.com>
2023-05-10 16:37:37 +0900

    update: SelectBox 기능수정 및 디자인수정 #53

--- components/SelectBox/index.tsx
+++ components/SelectBox/index.tsx
@@ -7,16 +7,20 @@ import {
 } from "./styled";
 import useOutsideClick from "../../hooks/useOutsideClick";
 
-interface SelectBoxProps {
-  options: Array<string | number>;
-  setValue: React.Dispatch<React.SetStateAction<any>>;
+interface OptionsType {
+  name: string;
   value: string | number;
+}
+interface SelectBoxProps {
+  options: OptionsType[];
+  setValue: React.Dispatch<React.SetStateAction<any>>;
   title?: string | number;
   size?: "small" | "medium" | "large";
 }
 
-function SelectBox({ options, setValue, value, title, size }: SelectBoxProps) {
+function SelectBox({ options, setValue, title, size }: SelectBoxProps) {
   const [isVisible, setIsVisible] = useState(false);
+  const [name, setName] = useState(title);
   const selectRef = useRef<HTMLDivElement>(null);
   useOutsideClick(selectRef, () => {
     setIsVisible(false);
@@ -24,25 +28,28 @@ function SelectBox({ options, setValue, value, title, size }: SelectBoxProps) {
   const handleSelectVisible = () => {
     setIsVisible(!isVisible);
   };
-  const handleOnChangeSelectValue = (e: React.MouseEvent<HTMLLIElement>) => {
-    const { innerText } = e.target as HTMLLIElement;
-    for (let i = 0; i < options.length; i++) {
-      if (typeof options[i] === "number") {
-        setValue(Number(innerText));
-      } else {
-        setValue(innerText);
-      }
-    }
+  const handleOnChangeSelectValue = <T extends string | number>(
+    value: T,
+    name: T,
+  ): void => {
+    setValue(value);
+    setName(name);
   };
+
   return (
     <SelectWrapper onClick={handleSelectVisible} ref={selectRef} size={size}>
-      <SelectValue>{value ? value : title}</SelectValue>
+      <SelectValue>{name ? name : title}</SelectValue>
       <SelectOptionWrapper visible={isVisible}>
         {options &&
-          options.map((option: string | number) => {
+          options.map((option) => {
             return (
-              <SelectOption key={option} onClick={handleOnChangeSelectValue}>
-                {option}
+              <SelectOption
+                key={option.name}
+                onClick={() =>
+                  handleOnChangeSelectValue(option.value, option.name)
+                }
+              >
+                {option.name}
               </SelectOption>
             );
           })}
@@ -52,8 +59,6 @@ function SelectBox({ options, setValue, value, title, size }: SelectBoxProps) {
 }
 export default React.memo(SelectBox);
 
-// export default SelectBox;
 //SelectBox 컴포넌트의 사용법은 부모컴포넌트로부터 4개의 props 를 받아야합니다.
-//options(셀렉트박스안에 들어갈 여러개의 값들의 객체or배열),value(부모컴포넌트의state),setValue(부모컴포넌트의 setState),title(첫 렌더링시 어떤 셀렉트박스인지 구분하기위한 props)
+//options 객체기 필요합니다. 객체의 양식은 {name"abcd",value:"abcd"}이며  name은 화면에 보여질 값 value 는 백엔드에 보낼 값입니다.
 //이 컴포넌트를 사용하는 페이지에서 useState를 사용하여 값을 관리해주시면 됩니다.
-//pages/userinfo/position.tsx 에서 사용법을 확인할 수 있습니다.
