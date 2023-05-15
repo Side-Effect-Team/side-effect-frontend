7ae02fa4f0be4904c3c2d321503e262be3bfb505
한주연 <hjy2859@naver.com>
2023-05-15 16:49:34 +0900

    feat: 마이페이지 탭 게시물 중 자랑,모집 게시글 필터 기능 구현 #48

--- components/pages/mypage/TabBoards/index.tsx
+++ components/pages/mypage/TabBoards/index.tsx
@@ -1,6 +1,7 @@
 import BoardCard, { BoardCardProps } from "@/components/BoardCard";
 import { Border, SectionHeaderWrapper, SectionTitle } from "../styled";
-import { BoardWrapper } from "./styled";
+import { BoardWrapper, FilterMenu, FilterWrapper, NullMessage } from "./styled";
+import { useEffect, useState } from "react";
 
 interface TabBoards {
   boards?: BoardCardProps[] | null;
@@ -8,7 +9,28 @@ interface TabBoards {
 }
 
 export default function TabBoards({ boards, title }: TabBoards) {
-  console.log(boards);
+  const [filterMenu, setFilterMenu] = useState("all");
+  const [filterBoards, setFilterBoards] = useState(boards);
+
+  const projectsBoards = boards?.filter(
+    (boards) => boards.category === "projects",
+  );
+  const recruitsBoards = boards?.filter(
+    (boards) => boards.category === "recruits",
+  );
+
+  const onClickFilterMenu = (type: string) => () => {
+    if (type === "projects") setFilterMenu("projects");
+    else if (type === "recruits") setFilterMenu("recruits");
+    else setFilterMenu("all");
+  };
+
+  useEffect(() => {
+    if (filterMenu === "projects") setFilterBoards(projectsBoards);
+    else if (filterMenu === "recruits") setFilterBoards(recruitsBoards);
+    else setFilterBoards(boards);
+  }, [filterMenu, boards]);
+
   return (
     <>
       {boards ? (
@@ -17,16 +39,43 @@ export default function TabBoards({ boards, title }: TabBoards) {
             <SectionTitle>{title}</SectionTitle>
             <Border></Border>
           </SectionHeaderWrapper>
+          {title !== "applyBoards" && (
+            <FilterWrapper>
+              <FilterMenu
+                onClick={onClickFilterMenu("all")}
+                isActive={filterMenu === "all"}
+              >
+                전체
+              </FilterMenu>
+              <FilterMenu
+                onClick={onClickFilterMenu("projects")}
+                isActive={filterMenu === "projects"}
+              >
+                자랑게시판
+              </FilterMenu>
+              <FilterMenu
+                onClick={onClickFilterMenu("recruits")}
+                isActive={filterMenu === "recruits"}
+              >
+                모집게시판
+              </FilterMenu>
+            </FilterWrapper>
+          )}
           <BoardWrapper>
-            {boards &&
-              boards.map((el, index) => <BoardCard key={index} data={el} />)}
+            {filterBoards &&
+              filterBoards.map((el, index) => (
+                <BoardCard key={index} data={el} />
+              ))}
           </BoardWrapper>
         </>
       ) : (
-        <SectionHeaderWrapper>
-          <SectionTitle>{title}</SectionTitle>
-          <Border></Border>
-        </SectionHeaderWrapper>
+        <>
+          <SectionHeaderWrapper>
+            <SectionTitle>{title}</SectionTitle>
+            <Border></Border>
+          </SectionHeaderWrapper>
+          <NullMessage>게시물이 없습니다.</NullMessage>
+        </>
       )}
     </>
   );
