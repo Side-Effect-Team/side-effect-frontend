3af2fbe9cdc711e20c164f19aed31167e5fe6fd6
jong-k <kjhan710@naver.com>
2023-05-19 19:12:12 +0900

    design: theme의 background color white(#fff)로 변경

--- styles/Theme.ts
+++ styles/Theme.ts
@@ -6,7 +6,7 @@ export const theme = {
     gray: "#98A2B3",
     danger: "#F04438",
     success: "#16B364",
-    background: "#f9f9f9",
+    background: "#fff",
     white: "#fff",
   },
   fonts: {
