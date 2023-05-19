047ae91c893153cc378376c4e7e0914630506a38
taejinii <xowls177@gmail.com>
2023-05-19 11:34:54 +0900

    chore: next-auth 관련코드삭제 #45

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import { AnimatePresence } from "framer-motion";
 import { Provider } from "react-redux";
-import { SessionProvider } from "next-auth/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { store } from "@/store/store";
@@ -12,23 +11,21 @@ const queryClient = new QueryClient();
 
 export default function App({
   Component,
-  pageProps: { session, ...pageProps },
+  pageProps: { ...pageProps },
 }: AppProps) {
   const router = useRouter();
   const pageKey = router.route;
   return (
     <QueryClientProvider client={queryClient}>
-      <SessionProvider session={session}>
-        <Provider store={store}>
-          <Layout>
-            <GlobalModal />
-            <AnimatePresence mode="wait">
-              <Component key={pageKey} {...pageProps} />
-            </AnimatePresence>
-          </Layout>
-        </Provider>
-        <ReactQueryDevtools />
-      </SessionProvider>
+      <Provider store={store}>
+        <Layout>
+          <GlobalModal />
+          <AnimatePresence mode="wait">
+            <Component key={pageKey} {...pageProps} />
+          </AnimatePresence>
+        </Layout>
+      </Provider>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
