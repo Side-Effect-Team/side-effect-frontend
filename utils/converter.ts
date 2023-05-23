0d0edd5f6ae47127fd4c8d162d5a3164d74849a8
jong-k <kjhan710@naver.com>
2023-05-23 21:20:34 +0900

    fix: recruit 게시글에 조회수 추가

--- utils/converter.ts
+++ utils/converter.ts
@@ -1,7 +1,16 @@
 // 모집 게시판 API의 반환 데이터를 BoardCard data props로 변환하는 함수
 export const recruitBoardCardConverter = (fetchedData: RecruitType) => {
-  const { id, content, tags, title, projectName, imgSrc, likeNum, createdAt } =
-    fetchedData;
+  const {
+    id,
+    content,
+    tags,
+    title,
+    projectName,
+    imgSrc,
+    likeNum,
+    createdAt,
+    views,
+  } = fetchedData;
   const newTags = tags ? tags.map((tag) => tag.stackType) : [];
   return {
     id,
@@ -13,5 +22,6 @@ export const recruitBoardCardConverter = (fetchedData: RecruitType) => {
     createdAt,
     like: !!Math.round(Math.random()), // 화면 확인을 위해 임시로 랜덤하게
     likeNum,
+    views,
   };
 };
