4d2830263364e5eefc049d2e8daa1e591e5a3bdb
taejinii <xowls177@gmail.com>
2023-05-27 17:29:02 +0900

    update: 로그아웃시 메모리에 남아있는 액세스토큰을 지우기위한 메서드추가 #93

--- utils/auth.ts
+++ utils/auth.ts
@@ -9,5 +9,9 @@ class HandleAuth {
   setToken(newToken: string) {
     this.token = newToken;
   }
+  //로그아웃시 메모리에 남아있는 토큰 제거
+  removeToken() {
+    this.token = "";
+  }
 }
 export const handleAuth = new HandleAuth();
