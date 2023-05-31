1643592ab188668b7503549a836c6d2b714fbe45
jong-k <kjhan710@naver.com>
2023-05-31 17:14:07 +0900

    refactor: 변경된 user persist 반영

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -4,21 +4,21 @@ import MobileMenuBox from "../MobileMenuBox";
 import { BOARD_LIST } from "../../enum";
 import Button from "../Button";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
-import { removeAuthentication } from "@/store/authSlice";
 import { openModal } from "@/store/modalSlice";
 import Alarm from "../Alarm";
 import { useRef, useState } from "react";
 import useOutsideClick from "../../hooks/common/useOutsideClick";
 
 interface HeaderProps {
+  logout: () => void;
   handleMobileMenu: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-export default function Header({ handleMobileMenu }: HeaderProps) {
+export default function Header({ logout, handleMobileMenu }: HeaderProps) {
   const [openAlarm, setOpenAlarm] = useState(false);
   const AlarmListRef = useRef(null);
   const dispatch = useAppDispatch();
-  const { authenticated } = useAppSelector((state) => state.auth);
+  const { token } = useAppSelector((state) => state.auth);
 
   useOutsideClick(AlarmListRef, () => setOpenAlarm(false));
 
@@ -36,13 +36,9 @@ export default function Header({ handleMobileMenu }: HeaderProps) {
           ))}
         </NavStyled>
         <BoxStyled>
-          {authenticated && (
-            <Alarm openAlarm={openAlarm} setOpenAlarm={setOpenAlarm} />
-          )}
-          {authenticated ? (
-            <Button onClick={() => dispatch(removeAuthentication())}>
-              로그아웃
-            </Button>
+          {token && <Alarm openAlarm={openAlarm} setOpenAlarm={setOpenAlarm} />}
+          {token ? (
+            <Button onClick={logout}>로그아웃</Button>
           ) : (
             <Button
               onClick={() => dispatch(openModal({ modalType: "LoginModal" }))}
