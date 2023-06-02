579553c047a85ee628f4237abdeebc23c1fba97b
jong-k <kjhan710@naver.com>
2023-06-02 16:34:44 +0900

    refactor: 프로젝트 상세 게시글 SSR 방식에서 CSR 방식으로 변경

--- pages/projects/[projectId].tsx
+++ pages/projects/[projectId].tsx
@@ -1,68 +1,65 @@
-import axios from "axios";
-import { GetServerSidePropsContext } from "next";
+import { useRouter } from "next/router";
+import { useQuery } from "@tanstack/react-query";
 import { Wrapper, Contents } from "@/postComps/common/PageLayout.styled";
 import ContentDetail from "@/detailComps/ContentDetail";
 import CommentBox from "@/detailComps/CommentBox";
 import PostData from "@/detailComps/PostData";
+import { getProjectPost } from "@/apis/ProjectAPI";
 
-interface ProjectDetailPageProps {
-  project: ProjectType;
-}
+export default function ProjectDetailPage() {
+  const router = useRouter();
+  const projectId = router.query.projectId as string;
 
-export default function ProjectDetailPage({ project }: ProjectDetailPageProps) {
-  const {
-    comments,
-    content,
-    createdAt,
-    id,
-    imgUrl,
-    likeNum,
-    projectName,
-    projectUrl,
-    title,
-    userId,
-    views,
-    writer,
-  } = project;
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["projectPost"],
+    queryFn: () => getProjectPost(projectId),
+    staleTime: 2000,
+  });
 
-  return (
-    <Wrapper>
-      <Contents>
-        <PostData
-          postId={id}
-          title={title}
-          createdAt={createdAt}
-          views={views}
-          likeNum={likeNum}
-          writerId={userId}
-          writer={writer}
-        />
-        <ContentDetail
-          projectName={projectName}
-          content={content}
-          projectUrl={projectUrl}
-          imgSrc={imgUrl}
-        />
-        <CommentBox boardId={id} comments={comments} />
-      </Contents>
-    </Wrapper>
-  );
-}
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
 
-export async function getServerSideProps(ctx: GetServerSidePropsContext) {
-  const projectId = ctx.params?.projectId;
-  const url = `${process.env.NEXT_PUBLIC_API_URL}/free-boards/${projectId}`;
-  try {
-    const res = await axios.get(url);
-    const project = await res.data;
+  if (isError) {
+    return <h2>데이터를 불러오는데 실패했습니다</h2>;
+  }
 
-    return {
-      props: {
-        project,
-      },
-    };
-  } catch (err) {
-    console.log(err);
-    return { notFound: true };
+  if (data) {
+    const {
+      comments,
+      content,
+      createdAt,
+      id,
+      imgUrl,
+      likeNum,
+      projectName,
+      projectUrl,
+      title,
+      userId,
+      views,
+      writer,
+    } = data.data;
+    return (
+      <Wrapper>
+        <Contents>
+          <PostData
+            postId={id}
+            title={title}
+            createdAt={createdAt}
+            views={views}
+            likeNum={likeNum}
+            writerId={userId}
+            writer={writer}
+          />
+          <ContentDetail
+            projectName={projectName}
+            content={content}
+            projectUrl={projectUrl}
+            imgSrc={imgUrl}
+          />
+          <CommentBox boardId={id} comments={comments} />
+        </Contents>
+      </Wrapper>
+    );
   }
 }
