f9e59aaffb4cfa4081c627e55a17949f5c606a3a
jong-k <kjhan710@naver.com>
2023-06-02 19:39:31 +0900

    feat: 마이 페이지로 연결되는 유저 프로필을 헤더에 추가

--- components/Header/LoggedInMenuBox/index.tsx
+++ components/Header/LoggedInMenuBox/index.tsx
@@ -1,38 +1,52 @@
-import {
-  BoxStyled,
-  LoggedInBox,
-} from "@/components/Header/GlobalNavBar/styled";
+import { useRouter } from "next/router";
+import { useQuery } from "@tanstack/react-query";
+import Image from "next/image";
 import Alarm from "@/components/Alarm";
-import Button from "@/components/Button";
-import { openModal } from "@/store/modalSlice";
 import { useAppSelector } from "@/store/hooks";
 import { useRef, useState } from "react";
 import useOutsideClick from "@/hooks/common/useOutsideClick";
+import { Container, ImgContainer } from "./styled";
+import { getMypageData } from "@/apis/UserAPI";
 
 export default function LoggedInMenuBox() {
   const AlarmListRef = useRef(null);
   const [openAlarm, setOpenAlarm] = useState(false);
   const { token } = useAppSelector((state) => state.auth);
+  const router = useRouter();
+
+  const { data } = useQuery({
+    queryKey: ["userProfileImg"],
+    queryFn: getMypageData,
+  });
 
   useOutsideClick(AlarmListRef, () => setOpenAlarm(false));
 
   return (
-    <BoxStyled ref={AlarmListRef}>
+    <div ref={AlarmListRef}>
       {token && (
-        <LoggedInBox>
-          <div>프로필</div>
+        <Container onClick={() => router.push("/mypage")}>
+          <ImgContainer>
+            {data?.data.imgUrl ? (
+              <Image
+                src={`${process.env.NEXT_PUBLIC_API_URL}/user/image/${data?.data.imgUrl}`}
+                alt={`${data.data.nickname}의 프로필 이미지`}
+                width={35}
+                height={35}
+                priority
+              />
+            ) : (
+              <Image
+                src={`${process.env.NEXT_PUBLIC_API_URL}/user/image/UserDefaultBackground.png`}
+                alt="기본 유저 이미지"
+                width={35}
+                height={35}
+                priority
+              />
+            )}
+          </ImgContainer>
           <Alarm openAlarm={openAlarm} setOpenAlarm={setOpenAlarm} />
-        </LoggedInBox>
+        </Container>
       )}
-      {token ? (
-        <Button onClick={logout}>로그아웃</Button>
-      ) : (
-        <Button
-          onClick={() => dispatch(openModal({ modalType: "LoginModal" }))}
-        >
-          로그인
-        </Button>
-      )}
-    </BoxStyled>
+    </div>
   );
 }
