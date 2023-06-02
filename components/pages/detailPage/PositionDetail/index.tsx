07683de3cd784f348bb030d5c3dec67b205e22ac
jong-k <kjhan710@naver.com>
2023-06-02 17:22:08 +0900

    feat: 모집 게시글 작성자에게 지원 현황 확인 버튼 표시

--- components/pages/detailPage/PositionDetail/index.tsx
+++ components/pages/detailPage/PositionDetail/index.tsx
@@ -1,19 +1,30 @@
+import { AiOutlineTeam } from "react-icons/ai";
 import { POSITION_LIST } from "enum";
-
 import { Wrapper, StyledHeader } from "./styled";
-
+import { useAppSelector } from "@/store/hooks";
 import PositionRow from "@/detailComps/PositionRow";
 
 interface PositionDetailProps {
+  writerId: number;
   positions: PositionType[];
 }
 
-export default function PositionDetail({ positions }: PositionDetailProps) {
-  console.log("포지션 정보", positions);
-
+export default function PositionDetail({
+  writerId,
+  positions,
+}: PositionDetailProps) {
+  const userId = useAppSelector((state) => +state.auth.userId);
   return (
     <Wrapper>
-      <StyledHeader>모집 포지션</StyledHeader>
+      <div>
+        <StyledHeader>모집 포지션</StyledHeader>
+        {writerId === userId && (
+          <span>
+            <AiOutlineTeam />
+          </span>
+        )}
+      </div>
+
       {positions.map((position) => {
         // 영문 포지션 이름을 한글로 바꿈
         let positionName = "";
