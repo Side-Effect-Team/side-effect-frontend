3c21cd7f952790052c34a7d97522c56c5139705a
한주연 <hjy2859@naver.com>
2023-06-05 17:46:20 +0900

    feat: 모집카드 모바일 뷰에서 화면 리사이징 시  태그가 숨겨지고 숨겨진 태그의 갯수를 계산하는 로직 구현 #109

--- components/Tag/TagBox/index.tsx
+++ components/Tag/TagBox/index.tsx
@@ -1,27 +1,68 @@
+import { useEffect, useRef, useState } from "react";
 import Tag from "..";
-import { TagContainer, TagTitle, TagWrapper } from "./styled";
+import { PlusTag, TagContainer, TagTitle, TagWrapper } from "./styled";
 interface TagArrayProps {
   tagArray: string[];
   title: string;
   fill?: string;
   fontSize?: string;
 }
+
 export default function TagBox({
   title,
   tagArray,
   fill,
   fontSize,
 }: TagArrayProps) {
+  const tagRef = useRef<HTMLDivElement>(null); // 태그가 차지할 수 있는 영역
+  const [hiddenTagCount, setHiddenTagCount] = useState(0);
+
+  const calculateTagWidths = () => {
+    const tagWrapper = tagRef.current;
+
+    if (tagWrapper) {
+      const TagWrapperWidth = tagWrapper.offsetWidth;
+      const tagsArray = Array.from(tagWrapper.children) as HTMLDivElement[];
+      let cumulativeWidth = 0;
+      let hiddenCount = 0;
+      tagsArray.forEach((tag) => {
+        // 각 태그 길이 계산
+        const tagWidth = tag.offsetWidth;
+        cumulativeWidth += tagWidth + 7;
+
+        // 길이가 넘어가는 순간 상태 변화
+        if (cumulativeWidth - 7.5 > TagWrapperWidth) {
+          hiddenCount++;
+          setHiddenTagCount(hiddenCount);
+        } else if (cumulativeWidth <= TagWrapperWidth) {
+          setHiddenTagCount(0);
+        }
+      });
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", calculateTagWidths);
+    return () => {
+      window.removeEventListener("resize", calculateTagWidths);
+    };
+  }, []);
+
   return (
     <TagContainer>
       <TagTitle>{title}</TagTitle>
-      <TagWrapper>
+      <TagWrapper ref={tagRef}>
         {tagArray.map((el, index) => (
           <Tag key={index} fill={fill} fontSize={fontSize}>
-            {el}{" "}
+            {el}
           </Tag>
         ))}
       </TagWrapper>
+      {hiddenTagCount ? (
+        <PlusTag fill={fill || "true"}>+ {hiddenTagCount}</PlusTag>
+      ) : (
+        <></>
+      )}
     </TagContainer>
   );
 }
--- components/Tag/TagBox/styled.ts
+++ components/Tag/TagBox/styled.ts
@@ -1,5 +1,5 @@
 import { media } from "@/styles/mediatest";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const TagContainer = styled.div`
   ${media.mobile} {
@@ -13,6 +13,7 @@ export const TagContainer = styled.div`
   }
 `;
 export const TagTitle = styled.div`
+  min-width: 50px;
   font-size: 14px;
   font-weight: 600;
   color: ${(p) => p.theme.colors.gray};
@@ -22,9 +23,37 @@ export const TagTitle = styled.div`
     margin-right: 10px;
   }
 `;
+export const PlusTag = styled.div<{ fill: string }>`
+  display: none;
+  ${media.mobile} {
+    display: block;
+    position: absolute;
+    right: 0;
+    top: 0;
+    font-size: 12px;
+    font-weight: 700;
+    text-align: center;
+    min-width: 22px;
+    padding: 2px;
+    border-radius: 50%;
+    ${(p) =>
+      p.fill === "true"
+        ? css`
+            color: white;
+            background-color: ${(p) => p.theme.brandColor.primary};
+            border: 2px solid ${(p) => p.theme.brandColor.primary};
+          `
+        : css`
+            color: ${(p) => p.theme.brandColor.primary};
+            background-color: ${(p) => p.theme.componentBgColor};
+            border: 2px solid ${(p) => p.theme.brandColor.primary};
+          `};
+  }
+`;
 
 export const TagWrapper = styled.div`
   width: 100%;
+
   height: 65px;
   display: flex;
   align-items: flex-start;
@@ -33,9 +62,8 @@ export const TagWrapper = styled.div`
   margin-bottom: 20px;
   overflow: hidden;
   ${media.mobile} {
-    width: calc(100% - 100px);
+    width: calc(100% - 90px);
     height: 25px;
-    overflow: hidden;
     margin-bottom: 0px;
   }
 `;
