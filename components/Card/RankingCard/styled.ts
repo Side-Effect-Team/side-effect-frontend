2a4742eda48ebf7d68f3250f6e88d13e106b6ded
taejinii <xowls177@gmail.com>
2023-06-05 13:01:09 +0900

    design: border-radius 다른카드들과통일 #107

--- components/Card/RankingCard/styled.ts
+++ components/Card/RankingCard/styled.ts
@@ -12,7 +12,7 @@ export const StyledLink = styled(Link)`
   min-height: 400px;
   position: relative;
   overflow: hidden;
-  border-radius: 35px;
+  border-radius: 15px;
   box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
   &:hover {
     & span {
@@ -25,7 +25,7 @@ export const StyledImage = styled(Image)`
   width: 100%;
   max-height: 400px;
   object-fit: cover;
-  border-radius: 35px;
+  border-radius: 15px;
   &:hover {
     transition: all 0.5s;
     transform: scale(1.1);
