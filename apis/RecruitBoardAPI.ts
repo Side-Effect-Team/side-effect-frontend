a65ae76c72e8b214ca0c1e513a78b798fee5734a
jong-k <kjhan710@naver.com>
2023-06-06 15:38:24 +0900

    feat: 모집 게시판 다수의 게시글 페칭 api 추가

--- apis/RecruitBoardAPI.ts
+++ apis/RecruitBoardAPI.ts
@@ -8,5 +8,21 @@ export const addLikeRecruit = async (id: number) => {
 // 상세 게시글 하나 받아오기
 export const getRecruitPost = async (postId: string) => {
   const response = await customAxios.get(`/recruit-board/${postId}`);
-  return response;
+  return response.data;
+};
+
+// 상세 게시글 여러개 받아오기
+export const getRecruits = async (
+  size: number,
+  stackType: string,
+  keyword: string,
+  lastId: number,
+) => {
+  let url = `/recruit-board/scroll?size=${size}`;
+  if (stackType) url += `&stackType=${stackType}`;
+  if (keyword) url += `&keyword=${keyword}`;
+  if (lastId && lastId > 1) url += `&lastId=${lastId}`;
+  console.log(url);
+  const response = await customAxios.get(url);
+  return response.data;
 };
