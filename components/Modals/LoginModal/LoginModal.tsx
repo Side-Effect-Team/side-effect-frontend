0f9ee34485f5841e4bcb2b9ef7376a0f9ecab8e5
taejinii <xowls177@gmail.com>
2023-06-06 16:15:43 +0900

    fix: 로그인상태가 아니라면 무조건 로그인첫단계부터 시작하도록 수정하였음 #112

--- components/Modals/LoginModal/LoginModal.tsx
+++ components/Modals/LoginModal/LoginModal.tsx
@@ -7,6 +7,7 @@ import RegisterUserInfo from "./RegisterView/RegisterUserInfo";
 import Login from "./Login";
 import RegisterSuccess from "./RegisterView/RegisterSuccess";
 import { AnimatePresence } from "framer-motion";
+import { handleModalView } from "@/store/loginViewTransitionSlice";
 const VIEW_COMPONENTS = {
   startLogin: <Login />,
   registerNickname: <RegisterNickname />,
@@ -17,14 +18,20 @@ export default function LoginModal() {
   const dispatch = useAppDispatch();
   const { isOpen, modalType } = useAppSelector((state) => state.modal);
   const { modalView } = useAppSelector((state) => state.loginView);
+  const { token } = useAppSelector((state) => state.auth);
   const handleModalClose = () => {
     dispatch(closeModal());
   };
   const handleViewRender = () => {
     return VIEW_COMPONENTS[modalView];
   };
+  // 모달들을 isOpen 하나의 상태로 열고닫기를 관리하기때문에 만약 두개 세개의 모달이 한페이지에 있다면 어떤모달을 열었는지 판단해주는코드.
   if (modalType !== "LoginModal") return null;
 
+  //토큰이없다면 즉 로그인상태가 아니라면 로그인모달을 열었을시 로그인 첫단계부터 시작.
+  if (!token) {
+    dispatch(handleModalView({ modalView: "startLogin" }));
+  }
   return (
     <Wrapper isOpen={isOpen}>
       <Header>
