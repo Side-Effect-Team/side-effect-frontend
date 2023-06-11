3567b9bd0dc83a5de074dfc094f5079b2a0ecb41
한주연 <hjy2859@naver.com>
2023-06-11 16:52:40 +0900

    feat: landing second 페이지 구현 #126

--- components/pages/landing/second/index.tsx
+++ components/pages/landing/second/index.tsx
@@ -1,8 +1,29 @@
 import { SectionContainer } from "../styled";
 
 import { useInView } from "react-intersection-observer";
+import { MainText, SubText, SubWrapper, Wrapper } from "./styled";
 export default function SecondSection() {
   const { ref, inView } = useInView({ threshold: 0.5 });
 
-  return <SectionContainer ref={ref}></SectionContainer>;
+  return (
+    <SectionContainer ref={ref}>
+      <Wrapper>
+        <MainText style={{ display: inView ? "block" : "none" }}>
+          사이드이펙트에서
+          <br /> 프로젝트 모집부터 결과물까지 <br />
+          한번에 관리해보세요!
+        </MainText>
+        <SubWrapper>
+          <SubText style={{ fontWeight: "bolder" }}>
+            MAKE TEAM, SHOW YOUR PROJECT
+          </SubText>
+          <SubText>
+            지금까지와는 다른 프로젝트 모집 플랫폼
+            <br />
+            사이트 이펙트와 함께 커리어를 만들어가요
+          </SubText>
+        </SubWrapper>
+      </Wrapper>
+    </SectionContainer>
+  );
 }
