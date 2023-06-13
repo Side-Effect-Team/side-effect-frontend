3f14f6b6556104153aa9fc93c9d070eab802ac33
jong-k <kjhan710@naver.com>
2023-06-13 19:21:01 +0900

    feat: 모바일 메뉴 박스에 hamburger icon 적용

--- components/Header/MobileMenuBox/index.tsx
+++ components/Header/MobileMenuBox/index.tsx
@@ -1,19 +1,18 @@
-import { IoReorderThree } from "react-icons/io5";
 import { Wrapper } from "./styled";
-import Button from "components/Button";
+import HamburgerIcon from "../HamburgerIcon";
+
+export type HandleMobileMenu = (e: React.MouseEvent<HTMLButtonElement>) => void;
 
 interface MobileMenuBoxProps {
-  handleMobileMenu: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  handleMobileMenu: HandleMobileMenu;
 }
 
 export default function MobileMenuBox({
   handleMobileMenu,
 }: MobileMenuBoxProps) {
   return (
-    <Wrapper>
-      <Button onClick={handleMobileMenu}>
-        <IoReorderThree />
-      </Button>
+    <Wrapper onClick={handleMobileMenu}>
+      <HamburgerIcon />
     </Wrapper>
   );
 }
--- components/Header/MobileMenuBox/styled.ts
+++ components/Header/MobileMenuBox/styled.ts
@@ -1,7 +1,10 @@
 import styled from "styled-components";
 import { media } from "styles/media";
+import { type HandleMobileMenu } from "./index";
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.button<{
+  onClick: HandleMobileMenu;
+}>`
   ${media.mobile} {
     cursor: pointer;
     display: flex;
@@ -9,5 +12,7 @@ export const Wrapper = styled.div`
     align-items: center;
   }
 
+  background: transparent;
+  border: 0;
   display: none;
 `;
