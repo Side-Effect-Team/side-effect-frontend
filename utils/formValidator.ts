dda43d4afb1f09c0bdfc0985bfd7ce1010d3bf32
jong-k <kjhan710@naver.com>
2023-06-14 19:52:15 +0900

    refactor: form validator를 유틸 함수로 분리

---
+++ utils/formValidator.ts
@@ -0,0 +1,47 @@
+import { PROJECT_POST_FORM, RECRUIT_POST_FORM } from "enum";
+
+const formValidator = (
+  postForm: typeof PROJECT_POST_FORM | typeof RECRUIT_POST_FORM,
+) => {
+  const newErrorMsgs = { ...postForm };
+
+  // 게시글 제목
+  if (postForm.title.trim().length < 5)
+    newErrorMsgs.title = "게시글 제목은 5자 이상 입력해야 합니다";
+
+  // 프로젝트명
+  if (
+    postForm.projectName.trim().length < 3 ||
+    postForm.projectName.trim().length > 20
+  )
+    newErrorMsgs.projectName = "프로젝트명은 3~20자 이내로 입력해주세요";
+
+  if (isProjectPostForm(postForm) && isProjectPostForm(newErrorMsgs)) {
+    // 한 줄 소개
+    if (
+      postForm.projectSubTitle.trim().length < 3 ||
+      postForm.projectSubTitle.trim().length > 30
+    )
+      newErrorMsgs.projectSubTitle =
+        "프로젝트 한 줄 소개는 3~30자 이내로 입력해주세요";
+
+    // 프로젝트 URL
+    if (postForm.projectUrl.trim().length === 0)
+      newErrorMsgs.content = "프로젝트 URL을 입력해주세요";
+  }
+
+  // 상세 내용
+  if (postForm.content.trim().length < 20)
+    newErrorMsgs.content = "게시글 내용은 20자 이상 입력해야 합니다";
+
+  return newErrorMsgs;
+};
+
+// 타입 가드
+const isProjectPostForm = (
+  postForm: any,
+): postForm is typeof PROJECT_POST_FORM => {
+  return postForm.projectSubTitle !== undefined;
+};
+
+export default formValidator;
