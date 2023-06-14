b981e106b68c30e4b59f44172a7264dfbb29852c
jong-k <kjhan710@naver.com>
2023-06-14 10:55:01 +0900

    feat: form page layout 스타일 추가

---
+++ components/Form/common/PageLayout.styled.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { sizes } from "styles/media";
+
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  background: ${(p) => p.theme.mainBackGround};
+`;
+
+export const Contents = styled.div`
+  margin: 0 auto;
+  padding: 1.5rem 1rem;
+  max-width: ${sizes.desktop}px;
+`;
