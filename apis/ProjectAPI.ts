d90f815135edcb32ac2c1822dd2694dbf3c154c2
jong-k <kjhan710@naver.com>
2023-06-14 19:50:36 +0900

    refactor: post submit 로직을 apis 폴더로 분리

--- apis/ProjectAPI.ts
+++ apis/ProjectAPI.ts
@@ -1,4 +1,6 @@
 import customAxios from "./customAxios";
+import { NextRouter } from "next/router";
+import { PROJECT_POST_FORM } from "enum";
 
 export const getProjectData = async (
   page: number,
@@ -15,4 +17,29 @@ export const getProjectData = async (
 export const getProjectPost = async (postId: string) => {
   const response = await customAxios.get(`/free-boards/${postId}`);
   return response;
+};
+
+// 프로젝트 자랑 글 등록
+export const submitProjectPost = async (
+  data: typeof PROJECT_POST_FORM,
+  uploadImg: Function,
+  router: NextRouter,
+) => {
+  // request
+  const url = `/free-boards`;
+  try {
+    const res = await customAxios.post(url, data);
+    const projectId = await res.data.id;
+    const imgUrl = url + "/image/" + projectId;
+
+    // image upload
+    if (res.status === 200) {
+      await uploadImg(imgUrl);
+      await window.alert("게시글 등록이 완료되었습니다");
+      await router.push(`/projects/${projectId}`);
+    }
+  } catch (err) {
+    console.log(err);
+    window.alert("게시글 등록에 실패했습니다");
+  }
 };
