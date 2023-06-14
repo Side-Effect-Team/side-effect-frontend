473284e1307e6c0d24e174d0c89a50ef41f30617
jong-k <kjhan710@naver.com>
2023-06-14 14:02:58 +0900

    refactor: 다크모드 토글 스위치를 푸터로 변경

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -9,7 +9,6 @@ import BatchCarousel from "components/Carousel/BatchCarousel";
 import ProjectList from "components/pages/project/ProjectLIst";
 import ProjectCardSkeleton from "components/Skeleton/ProjectCardSkeleton";
 import { useAppSelector } from "store/hooks";
-import DarkModeToggle from "components/DarkModeToggle";
 import { sizes } from "styles/media";
 
 const FILTER_OPTIONS = [
@@ -34,7 +33,6 @@ export default function ProjectPage() {
   }, [refetch, token]);
   return (
     <Wrapper>
-      <DarkModeToggle />
       <PageHead pageTitle="프로젝트 자랑 | 사이드 이펙트" />
       <BatchCarousel
         title="이달의 베스트 프로젝트"
