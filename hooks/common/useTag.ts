172175ee6e4f36e5b19d9777230cbff8bac2f751
jong-k <kjhan710@naver.com>
2023-06-16 17:37:04 +0900

    feat: useTag 훅스 로직 변경

--- hooks/common/useTag.ts
+++ hooks/common/useTag.ts
@@ -1,27 +1,36 @@
-import { KeyboardEvent, MouseEvent, useState } from "react";
+import { useState } from "react";
+import useToast from "./useToast";
 
 export const useTag = (initialVals: string[]) => {
   const [tags, setTags] = useState<string[]>([...initialVals]);
+  const { addToast } = useToast();
 
-  const deleteTag = (e: MouseEvent) => {
-    const spanEl = e.target as HTMLSpanElement;
-    setTags(tags.filter((tag) => tag !== spanEl.textContent));
+  const deleteTag = (value: string) => {
+    setTags(tags.filter((tag) => tag !== value));
   };
 
-  const addTag = (e: KeyboardEvent<HTMLInputElement>) => {
-    const inputEl = e.target as HTMLInputElement;
-    // 아무것도 없는 상태에서 엔터키 누르면 종료
-    if (inputEl.value === "") return;
-
+  const addTag = (value: string) => {
     // 기존 태그 갯수가 5개이면 종료
-    if (tags.length >= 5) return;
-
-    if (e.key === "Enter") {
-      // 태그 추가 엔터 시 폼이 자동 등록되는 것 막기
-      e.preventDefault();
-      setTags([...tags, inputEl.value]);
-      inputEl.value = "";
+    if (tags.length >= 5) {
+      addToast({
+        type: "error",
+        title: "error",
+        content: "최대 5개까지의 태그를 입력할 수 있습니다",
+      });
+      return;
     }
+
+    // 중복된 태그 제출 시 종료
+    if (tags.includes(value)) {
+      addToast({
+        type: "error",
+        title: "error",
+        content: "이미 등록된 태그입니다",
+      });
+      return;
+    }
+
+    setTags([...tags, value]);
   };
 
   return { tags, setTags, deleteTag, addTag };
